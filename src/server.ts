1c224fec4424ae4b0be98427510ca446b8a66717
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-17 20:36:17 -0300

    fix: correct spelling of 'heroes' in routes and variable names

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-import { log } from "console";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 
@@ -9,7 +8,7 @@ server.register(cors, {
     methods: ["GET", "POST"],
 })
 
-const heros = [
+const heroes = [
     { id: 1, name: "Leon S. Kennedy", weapon: "handgun" },
     { id: 2, name: "Claire Redfield", weapon: "shotgun" },
     { id: 3, name: "Jill Valentine", weapon: "pistol" },
@@ -27,10 +26,10 @@ const villains = [
     { id: 6, name: "Jack Krauser", weapon: "cybernetic enhancements" }
 ]
 
-server.get("/heros", async (request, response) => {
+server.get("/heroes", async (request, response) => {
     response.type("application/json").code(200)
 
-    return heros;
+    return heroes;
 });
 
 server.get("/villains", async (request, response) => {
@@ -45,7 +44,7 @@ interface HeroParams {
 
 server.get<{ Params: HeroParams }>("/heros/:id", async (request, response) => {
     const id = parseInt(request.params.id);
-    const hero = heros.find(h => h.id === id);
+    const hero = heroes.find(h => h.id === id);
 
     if (!hero) {
         response.type("application/json").code(404);
